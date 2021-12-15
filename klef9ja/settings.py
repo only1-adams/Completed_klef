d22e196d7943fde293dadb0a7c0794714d5d554b
Adams Muhammed <muhammedadam4544@gmail.com>
2021-12-15 15:44:32 +0100

    firstt

--- klef9ja/settings.py
+++ klef9ja/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+k__l*59u#0fwcf&h%)ioi9#z3vb935d&&+6@xsx%0^wbj5-sx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
